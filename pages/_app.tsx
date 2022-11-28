489ce7803c6daba950f160d56396a18dcaef5e1c
nastaransharifisadr <4sadrn08@solent.ac.uk>
2022-11-28 21:54:02 +0000

    installing tailswindcss+ adding header and background and logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "../styles/globals.css";
+import Image from "next/image";
+import Link from "next/link";
+
+
 import type {AppProps} from "next/app";
 
-export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+  return (<div className='bg-orange-200 border-x-orange-800  min-h-screen'>
+
+      <nav className="p-7 bg-orange-300 flex justify-between border-b-2 border-t-2 border-x-orange-800 fix top-0 left-0 right-0 ">
+        <Link href="/">
+          <Image alt="logo" layout="fixed" src="/img/logo.png" width={102} height={46} className="cursor-pointer"></Image>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+     
+  
+  
+  
+  </div> );
 }
+export default MyApp;
---
+++ pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Room() {
+	return (
+		<div className="max-w-[80%] mx-auto mt-11">
+            
+	    </div>
+
+	)
+}
