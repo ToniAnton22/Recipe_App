550897b784626ee05a33badfe9cd5bd9522479be
nastaransharifisadr <4sadrn08@solent.ac.uk>
2022-11-29 00:26:06 +0000

    Adding Banner component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 
 import type {AppProps} from "next/app";
 
-
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <div className="bg-orange-200 border-x-orange-800  min-h-screen">
-      <Header/>
+      <Header />
+      <Banner/>
+      
       <Component {...pageProps} />
+      
+   
     </div>
   );
 }
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,10 @@
-export default function Room() {
-  return <div className="max-w-[80%] mx-auto mt-11"></div>;
+
+
+
+function home() {
+  return (
+    <div>home</div>
+  )
 }
+
+export default home
